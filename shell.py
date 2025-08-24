cd1647ba0839a28a8ac8f1af41940ff6ef9ccf80
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-08-24 18:35:07 -0400

    Add initial implementation of lexer and tokenization logic

---
+++ shell.py
@@ -0,0 +1,15 @@
+import paw
+
+BLUE = "\033[34m"
+RESET = "\033[0m"
+RED = "\033[31m"
+GREEN = "\033[32m"
+PURPLE = "\033[35m"
+BOLD = "\033[1m"
+
+while True:
+    text = input(f'{BLUE}paw {RESET}{BOLD}>{RESET} ')
+    result, error = paw.run(text)
+
+    if error: print(f'{RED}{error.as_string()}{RESET}')
+    else: print(result)
